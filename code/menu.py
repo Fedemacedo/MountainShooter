e3c80fc154ccd1ad5e83fe65a446bb07ee87d27d
Felipe <lipe6nunes@gmail.com>
2025-03-18 21:40:19 -0300

    Changing menu

--- code/menu.py
+++ code/menu.py
@@ -18,8 +18,8 @@ class Menu:
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, 'Mountain', (COLOR_ORANGE), ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, 'Shooter', (COLOR_ORANGE), ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, 'Mountain', (COLOR_WHITE), ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, 'Shooter', (COLOR_WHITE), ((WIN_WIDTH / 2), 120))
             self.menu_text(20, 'NEW GAME 1P', (COLOR_WHITE), ((WIN_WIDTH / 2), 200))
             self.menu_text(20, 'NEW GAME 2P - COOPERATIVE', (COLOR_WHITE), ((WIN_WIDTH / 2), 225))
             self.menu_text(20, 'NEW GAME 2P - COMPETITIVE', (COLOR_WHITE), ((WIN_WIDTH / 2), 250))
